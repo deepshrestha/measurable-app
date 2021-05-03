96a7d20cb5b9bbbebf235c8a99c6316cacb01e9b
Rushan Tamrakar <rushan2072@gmail.com>
2021-05-03 10:52:49 +0545

    new design implementation

--- src/components/DeviceDetails/Child/Charts.js
+++ src/components/DeviceDetails/Child/Charts.js
@@ -1,23 +1,295 @@
 import React from "react";
+import { Bar, Line } from "react-chartjs-2";
 
 export default function Charts() {
+  const barChartData = {
+    labels: [
+      "00:00",
+      "01:00",
+      "02:00",
+      "03:00",
+      "04:00",
+      "05:00",
+      "06:00",
+      "07:00",
+      "08:00",
+      "09:00",
+      "10:00",
+      "11:00",
+      "12:00",
+      "13:00",
+      "14:00",
+      "15:00",
+      "16:00",
+      "17:00",
+      "18:00",
+      "19:00",
+      "20:00",
+      "21:00",
+      "22:00",
+      "23:00",
+    ],
+    datasets: [
+      {
+        data: [3.7, 3.7, 3.7, 3.8, 3.7, 3.7, 3.6, 3.6, 3.42],
+        backgroundColor: [
+          "#FF8C2E",
+          "#FF8C2E",
+          "#FF8C2E",
+          "#FF8C2E",
+          "#FF8C2E",
+          "#FF8C2E",
+          "#FF8C2E",
+          "#FF8C2E",
+          "#FF8C2E",
+        ],
+      },
+    ],
+  };
+
+  const options = {
+    plugins: {
+      legend: false,
+    },
+  };
+
+  const lineChartData = {
+    labels: [
+      "0",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "11",
+      "12",
+      "13",
+      "14",
+      "15",
+      "16",
+      "17",
+      "18",
+      "19",
+      "20",
+      "21",
+      "22",
+      "23",
+      "24",
+      "25",
+      "26",
+      "27",
+      "28",
+      "29",
+      "30",
+      "31",
+      "32",
+      "33",
+      "34",
+      "35",
+      "36",
+      "37",
+      "38",
+      "39",
+      "40",
+      "41",
+      "42",
+      "43",
+      "44",
+      "45",
+      "46",
+      "47",
+      "48",
+      "49",
+      "50",
+      "51",
+      "52",
+      "53",
+      "54",
+      "55",
+      "56",
+      "57",
+      "58",
+      "59",
+    ],
+    datasets: [
+      {
+        label: "left_outlet(latest hour)",
+        data: [
+          0,
+          0.1,
+          0,
+          0.1,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0.1,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+        ],
+        borderColor: ["#90ED7D"],
+        backgroundColor: ["#90ED7D"],
+      },
+      {
+        label: "right_outlet(latest hour)",
+        data: [
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+          3.6,
+        ],
+        borderColor: ["#7DAEED"],
+        backgroundColor: ["#7DAEED"],
+      },
+    ],
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  };
+
   return (
     <>
       <div className="row">
         <div className="col-sm-12">
           <div className="metric-box chart">
             <p className="text-left">Energy(Wh) - Apr 29, 2021</p>
-            <canvas id="barChart" width="400" height="50"></canvas>
+            <Bar
+              data={barChartData}
+              width="400"
+              height="50"
+              options={options}
+            />
           </div>
 
           <div className="metric-box chart">
             <p className="text-left">Power (Watts) 08:00 - 09:00</p>
-            <canvas id="lineChart" width="400" height="50"></canvas>
+            <Line
+              data={lineChartData}
+              width={400}
+              height={50}
+              options={{ maintainAspectRatio: true }}
+            />
           </div>
 
           <div className="metric-box chart">
             <p className="text-left">Power (Watts) 08:50 - 08:59</p>
-            <canvas id="lineChart1" width="400" height="50"></canvas>
+            <Line
+              data={lineChartData}
+              width={400}
+              height={50}
+              options={{ maintainAspectRatio: true }}
+            />
           </div>
         </div>
       </div>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,80 +1,88 @@
 import React, { useEffect, useRef, useState } from "react";
-import { useHistory } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 const Login = () => {
-    let inputRef = useRef();
-    let history = useHistory();
+  let inputRef = useRef();
+  let history = useHistory();
 
-    let initialState = {
-        email: null,
-        password: null
-    };
+  let initialState = {
+    email: null,
+    password: null,
+  };
 
-    const [formState, setFormState] = useState(initialState);
+  const [formState, setFormState] = useState(initialState);
 
-    const onHandleChange = event => {
-        event.preventDefault();
-        const { name, value } = event.target;
-        setFormState({
-            ...formState,
-            [name]: value
-        });
-    };
+  const onHandleChange = (event) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
 
-    useEffect(() => {
-        inputRef.current.focus();
-    }, []);
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
 
-    const onLogin = event => {
-        event.preventDefault();
+  const onLogin = (event) => {
+    event.preventDefault();
 
-        const { email, password } = formState;
+    const { email, password } = formState;
 
-        if (email === "sid@organicecommerce.com" && password === "12345678") {
-            if (!window.localStorage.getItem("user_name"))
-                window.localStorage.setItem("user_name", "Sid");
+    if (email === "sid@organicecommerce.com" && password === "12345678") {
+      if (!window.localStorage.getItem("user_name"))
+        window.localStorage.setItem("user_name", "Sid");
 
-            history.push("/home");
-        } else {
-            alert("Login credential is not matched!");
-        }
-    };
+      history.push("/home");
+    } else {
+      alert("Login credential is not matched!");
+    }
+  };
 
-    return (
-        <div className="text-center" style={{margin: "100px"}}>
-            <form className="form-label" onSubmit={(event) => onLogin(event)}>
-                <h3>Please sign in</h3>
-                <div className="row">
-                    <div className="co-md-12" style={{margin: "5px"}}>
-                        <input 
-                            ref={inputRef}
-                            type="email" 
-                            name="email"
-                            className="form-control-md"
-                            style={{width: "20%"}}
-                            placeholder="Email address" 
-                            onChange={onHandleChange} />
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-12">
-                        <input 
-                            type="password" 
-                            name="password" 
-                            minLength="3"
-                            maxLength="10"
-                            className="form-control-md" 
-                            style={{width: "20%"}}
-                            placeholder="Password" 
-                            onChange={onHandleChange} />
-                    </div> 
-                </div>
-                <div style={{margin: "10px"}}>
-                    <button className="btn btn-sm btn-primary">Sign in</button>
-                </div>
-            </form>
+  return (
+    <div className="d-flex login-container">
+      <div className="container">
+        <div className="login-form">
+          <figure></figure>
+          <form onSubmit={(event) => onLogin(event)}>
+            <h2>Welcome Back!</h2>
+            <div className="form-group">
+              <label className="form-label">Email</label>
+              <input
+                ref={inputRef}
+                type="email"
+                name="email"
+                className="form-control"
+                placeholder="Email address"
+                onChange={onHandleChange}
+              />
+            </div>
+            <div className="form-group">
+              <label className="form-label">Password</label>
+              <input
+                type="password"
+                name="password"
+                minLength="3"
+                maxLength="10"
+                className="form-control"
+                placeholder="Password"
+                onChange={onHandleChange}
+              />
+            </div>
+            <div className="form-group d-flex justify-content-between">
+              <label className="remember">
+                <input type="checkbox" name="remember_me" />
+                Remember Me
+              </label>
+              <Link to="./#">Forgot Your Password?</Link>
+            </div>
+            <button className="btn btn-primary">Sign in</button>
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
